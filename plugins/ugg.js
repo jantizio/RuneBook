0acaf246003b7f89a9422a599c373c5a60ca58fc
Rerago <65850767+Rerago@users.noreply.github.com>
2020-10-21 10:08:27 +0200

    rework u.gg
    - support of different games modes
    - support of all roles
    - completely rewritten from scratch

--- plugins/ugg.js
+++ plugins/ugg.js
@@ -1,196 +1,297 @@
-const { map } = require('lodash');
-const { getJson, sortRunes } = require('./utils');
+const rp = require('request-promise-native');
+const {
+    sortRunes
+} = require('./utils');
 
-// U.GG API consts
-// data[servers][tiers][positions][0][stats][perks/shards]
+// #region U.GG API consts
 const u = {
-  positions: {
-    jungle: 1,
-    support: 2,
-    adc: 3,
-    top: 4,
-    mid: 5
-  },
-  positionsReversed: {
-    1: 'Jungle',
-    2: 'Support',
-    3: 'ADC',
-    4: 'Top',
-    5: 'Mid'
-  },
-  tiers: {
-    challenger: 1,
-    master: 2,
-    diamond: 3,
-    platinum: 4,
-    gold: 5,
-    silver: 6,
-    bronze: 7,
-    overall: 8,
-    platPlus: 10,
-    diaPlus: 11
-  },
-  servers: {
-    na: 1,
-    euw: 2,
-    kr: 3,
-    eune: 4,
-    br: 5,
-    las: 6,
-    lan: 7,
-    oce: 8,
-    ru: 9,
-    tr: 10,
-    jp: 11,
-    world: 12
-  },
-  stats: {
-    perks: 0,
-    statShards: 8
-  },
-  perks: {
-    games: 0,
-    won: 1,
-    mainPerk: 2,
-    subPerk: 3,
-    perks: 4
-  },
-  shards: {
-    games: 0,
-    won: 1,
-    stats: 2
-  }
+    positions: {
+        jungle: 1,
+        support: 2,
+        adc: 3,
+        top: 4,
+        mid: 5,
+        none: 6
+    },
+    positionsReversed: {
+        1: 'Jungle',
+        2: 'Support',
+        3: 'ADC',
+        4: 'Top',
+        5: 'Mid',
+        6: ''
+    },
+    tiers: {
+        challenger: 1,
+        master: 2,
+        diamond: 3,
+        platinum: 4,
+        gold: 5,
+        silver: 6,
+        bronze: 7,
+        overall: 8,
+        platPlus: 10,
+        diaPlus: 11
+    },
+    servers: {
+        na: 1,
+        euw: 2,
+        kr: 3,
+        eune: 4,
+        br: 5,
+        las: 6,
+        lan: 7,
+        oce: 8,
+        ru: 9,
+        tr: 10,
+        jp: 11,
+        world: 12
+    },
+    stats: {
+        perks: 0,
+        statShards: 8
+    },
+    perks: {
+        games: 0,
+        won: 1,
+        mainPerk: 2,
+        subPerk: 3,
+        perks: 4
+    },
+    shards: {
+        games: 0,
+        won: 1,
+        stats: 2
+    }
 };
+// #endregion
 
-// KEY CONSTS - UPDATE THESE ACCORDING TO GUIDE https://gist.github.com/paolostyle/fe8ce06313d3e53c134a24762b9e519c
-const uGGAPIVersion = '1.1';
+// #region Settings
+const supported_modes = [{
+        key: "ranked_solo_5x5",
+        name: "Ranked",
+        tier: u.tiers.platPlus
+    },
+    {
+        key: "normal_aram",
+        name: "ARAM",
+        tier: u.tiers.overall
+    }
+];
 
-const server = u.servers.world;
-const tier = u.tiers.platPlus;
+const baseOverviewUrl = `https://stats2.u.gg/lol`;
+const statsVersion = "1.1";
+const overviewVersion = "1.4.0";
+const usedServer = u.servers.world;
+// #endregion
 
-const getUGGFormattedLolVersion = lolVer =>
-  lolVer
-    .split('.')
-    .splice(0, 2)
-    .join('_');
+/**
+ * Get the Overview-JSON from the U.GG-API.
+ * 
+ * @param {number} championId Id of the champion for which the data should be determined.
+ * @param {string} gameMode The game mode for which the rune pages should be determined.
+ * @returns The full Overview-JSON for the champion in the specified game mode.
+ */
+async function getOverviewJson(championId, gameMode) {
+    // Try the last two LOL versions (this is necessary because U.GG is currently updating with a delay on Patch-Day)
+    for (const lolVersion of freezer.get().lolversions.slice(0, 2)) {
+        // Convert LOL version into a format suitable for U.GG
+        const uggLoLVersion = lolVersion.split('.').splice(0, 2).join('_');
 
-function extractPage(champion) {
-  return (item, key) => {
-    const perksData = item[0][u.stats.perks];
-    const statShards = item[0][u.stats.statShards][u.shards.stats].map(str => parseInt(str, 10));
+        // Create URL based on the parameters
+        const requestUri = `${baseOverviewUrl}/${statsVersion}/overview/${uggLoLVersion}/${gameMode}/${championId}/${overviewVersion}.json`;
 
-    const primaryStyleId = perksData[u.perks.mainPerk];
-    const subStyleId = perksData[u.perks.subPerk];
+        // Query URL and return result
+        return await rp({
+                uri: requestUri,
+                json: true
+            })
+            .then(function(response) {
+                return response;
+            })
+            .catch(function(err) {
+                throw Error("Error when determining json => " + err);
+            });
+    }
 
-    const selectedPerkIds = sortRunes(perksData[u.perks.perks], primaryStyleId, subStyleId).concat(
-      statShards
-    );
+    // fallback
+    return null;
+}
 
-    return {
-      name: `${champion} ${u.positionsReversed[key]}`,
-      primaryStyleId,
-      subStyleId,
-      selectedPerkIds,
-      games: perksData[u.perks.games],
-      bookmark: {
-        src: '',
-        meta: {
-          pageType: key,
-          champion
-        },
-        remote: {
-          name: 'U.GG',
-          id: 'ugg'
+/**
+ * Returns a rune page based on the JSON and with the given data.
+ * 
+ * @param {string} runesJson U.GG JSON that is being processed.
+ * @param {string} champion Name of the champion for which the page should be determined.
+ * @param {number} position Position for which the page should be determined.
+ * @param {string} gameMode The game mode for which the rune page should be determined.
+ * @returns A rune page matches the given parameters.
+ */
+function getPage(runesJson, champInfo, position, gameMode) {
+    try {
+        // Break Json down to the position and data
+        const runesJsonModed = runesJson[position][0];
+
+        // Break Json down to the perks data and stat shards
+        const perksData = runesJsonModed[u.stats.perks];
+        const statShards = runesJsonModed[u.stats.statShards][u.shards.stats].map(str => parseInt(str, 10));
+
+        // Get ID of primary style and sub styles
+        const primaryStyleId = perksData[u.perks.mainPerk];
+        const subStyleId = perksData[u.perks.subPerk];
+
+        // Determine selected perk ids
+        const selectedPerkIds = sortRunes(perksData[u.perks.perks], primaryStyleId, subStyleId).concat(
+            statShards
+        );
+
+        // Return rune page
+        return {
+            name: `[${gameMode.name}] ${champInfo.name} ${u.positionsReversed[position]}`.trim(),
+            primaryStyleId: primaryStyleId,
+            selectedPerkIds: selectedPerkIds,
+            subStyleId: subStyleId,
+            bookmark: {
+                champId: champInfo.id,
+                gameModeKey: gameMode.key,
+                position: position,
+                remote: {
+                    name: plugin.name,
+                    id: plugin.id
+                }
+            }
+        };
+    } catch (e) {
+        throw Error(e);
+    }
+}
+
+/**
+ * Determines all possible rune pages for a given champion for the specified game mode.
+ * 
+ * @param {string} champion Name of the champion for which the pages should be determined.
+ * @param {string} gameMode The game mode for which the rune pages should be determined.
+ * @param {number} force_position Returns the position to which the data is to be returned. In case of NULL all are returned.
+ * @returns all possible rune pages for a particular champion for the specified game mode.
+ */
+async function getPagesForGameMode(champInfo, gameMode, force_position = null) {
+    // Return variable (List of rune pages)
+    var returnVal = [];
+
+    try {
+        // get json for the given champ and game mode
+        var result = await getOverviewJson(champInfo.key, gameMode.key);
+
+        // if a result was found, parse it and add it to the return
+        if (result) {
+            // reduce returns by server and tier (json includes all servers and tiers)
+            result = result[usedServer][gameMode.tier];
+
+            // check if a certain position was selected. If not all are returned
+            if (force_position) {
+                // add only the rune page for the selected position
+                returnVal.push(getPage(result, champInfo, force_position, gameMode));
+            } else {
+                // run through all positions
+                for (const position of Object.keys(result)) {
+                    returnVal.push(getPage(result, champInfo, position, gameMode));
+                }
+            }
         }
-      }
-    };
-  };
+    } catch (e) {
+        throw Error(e);
+    }
+
+    // Return list of the rune page
+    return returnVal;
 }
 
-async function getDataSource(champion, version = null) {
-  let returnVal = [];
-
-  // try last two lol-version
-  for (const lolVersion of freezer.get().lolversions.slice(0, 2)) {
-      try {
-          console.log(lolVersion);
-          let lolVersionUGG = getUGGFormattedLolVersion(lolVersion);
-
-          const overviewVersion = "1.4.0";
-          const championDataUrl = `https://static.u.gg/assets/lol/riot_static/${lolVersion}/data/en_US/champion.json`;
-          console.log(`U.GG DataUrl => ${championDataUrl}`);
-
-          const championData = await getJson(championDataUrl);
-          const championId = championData.data[champion].key;
-          console.log(`U.GG => ${lolVersionUGG}`);
-          const championStatsUrl = `https://stats2.u.gg/lol/${uGGAPIVersion}/overview/${lolVersionUGG}/ranked_solo_5x5/${championId}/${overviewVersion}.json`;
-          console.log(`U.GG ReqUrl => ${championStatsUrl}`);
-
-          returnVal = await getJson(championStatsUrl);
-
-          if (returnVal.length > 0)
-              break;
-      } catch (e) {
-          if (e.name === 'StatusCodeError' && e.statusCode == 403) {
-              continue;
-          } else {
-              throw Error(e);
-          }
-      }
-  };
-
-  return returnVal;
-}
-
-async function updateBookmark(champion, pageId, callback) {
-  try {
-    const championStats = await getDataSource(champion);
-    const page = extractPage(champion)(championStats[server][tier][pageId], pageId);
-    delete page.games;
-    callback(page);
-  } catch (e) {
-    callback({});
-    throw Error(e);
-  }
-}
-
+/**
+ * Determines all possible rune pages for a particular champion for all supported game modes.
+ * 
+ * @param {string} champion Id of the champion for which the pages should be determined.
+ * @param callback callback which is called for the return of the data.
+ * @returns all possible rune pages for a particular champion for all supported game modes.
+ */
 async function _getPages(champion, callback) {
-  const runePages = { pages: {} };
+    const runePages = {
+        pages: {}
+    };
 
-  try {
-    const championStats = await getDataSource(champion);
+    // Determine champion information based on the name
+    const champInfo = freezer.get().championsinfo[champion];
 
-    let pages = map(championStats[server][tier], extractPage(champion));
-    let totalGames = pages.reduce((total, current) => (total += current.games), 0);
+    try {
+        // go through all supported game modes
+        for (const gameMode of supported_modes) {
+            // determine rune pages for the respective game mode and add them to the return
+            for (const runePage of await getPagesForGameMode(champInfo, gameMode)) {
+                runePages.pages[runePage.name] = runePage;
+            }
+        }
 
-    pages = pages.filter(page => {
-      const positionPercentage = page.games / totalGames;
-      delete page.games;
-      return positionPercentage > 0.1;
-    });
+        // sort rune pages based on the key (name)
+        const ordered = {};
+        Object.keys(runePages.pages).sort().forEach(function(key) {
+            ordered[key] = runePages.pages[key];
+        });
+        runePages.pages = ordered;
 
-    pages.forEach(page => {
-      runePages.pages[page.name] = page;
-    });
-
-    callback(runePages);
-  } catch (e) {
-    callback(runePages);
-    throw Error(e);
-  }
+        // return rune pages
+        callback(runePages);
+    } catch (e) {
+        // In case of error, return all rune pages determined up to this point
+        callback(runePages);
+        throw Error(e);
+    }
 }
 
-const plugin = {
-  id: 'ugg',
-  name: 'U.GG',
-  active: true,
-  bookmarks: true,
-  getPages(champion, callback) {
-    _getPages(champion, callback);
-  },
-  syncBookmark(bookmark, callback) {
-    updateBookmark(bookmark.meta.champion, bookmark.meta.pageType, callback);
-  }
+/**
+ * It averages a special rune page based on the given parameters to update the bookmark.
+ * 
+ * @param {string} champId Id of the champion for which the rune page should be determined.
+ * @param {number} position Position for which the rune page should be determined.
+ * @param {string} gameModeKey Key of the game mode for which the rune page should be determined.
+ * @param callback callback which is called for the return of the data.
+ */
+async function _syncBookmark(champId, position, gameModeKey, callback) {
+    try {
+        // Determine champion information based on the name
+        const champInfo = freezer.get().championsinfo[champId];
+
+        // Determine game mode on the basis of the key
+        const gameMode = supported_modes.filter(i => i.key == gameModeKey)[0];
+
+        // determine all pages for the selected game mode and position
+        const pages = await getPagesForGameMode(champInfo, gameMode, position);
+
+        // return rune page
+        callback(pages[0]);
+    } catch (e) {
+        // If there is an error still callback so the UI does not hang
+        callback();
+        throw Error(e);
+    }
+}
+
+// #region Plugin-Funktionen
+var plugin = {
+    id: "ugg",
+    name: "UGG",
+    active: true,
+    bookmarks: true,
+
+    getPages(champion, callback) {
+        // Find all rune pages
+        _getPages(champion, callback);
+    },
+    syncBookmark(bookmark, callback) {
+        // Find and update a rune page based on the bookmark
+        _syncBookmark(bookmark.champId, bookmark.position, bookmark.gameModeKey, callback);
+    }
 };
 
-module.exports = { plugin };
+module.exports = {
+    plugin
+};
+// #endregion
