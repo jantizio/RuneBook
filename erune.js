31b758a48e17a8bf874120b6b6e4fbe68de5e2ff
jantizio <emanuele0246@gmail.com>
2021-09-07 11:30:28 +0200

    bug fix pagina principale = a secondaria

--- erune.js
+++ erune.js
@@ -3,7 +3,6 @@ const { template } = require('lodash');
 const p_rune = [8000, 8100, 8200, 8400, 8300]; //TODO: let var const
 rune =
   '{"8000":[[8005,8008,8021,8010],[9101,9111,8009],[9104,9105,9103],[8014,8017,8299]],"8100":[[8112,8124,8128,9923],[8126,8139,8143],[8136,8120,8138],[8135,8134,8105,8106]],"8200":[[8214,8229,8230],[8224,8226,8275],[8210,8234,8233],[8237,8232,8236]],"8400":[[8437,8439,8465],[8446,8463,8401],[8429,8444,8473],[8451,8453,8242]],"8300":[[8351,8360,8358],[8306,8304,8313],[8321,8316,8345],[8347,8410,8352]]}';
-//var label, img, radio, c;
 var srs = []; //2 secondary runes line name
 var rune = JSON.parse(rune);
 /*const rune = {
@@ -85,8 +84,10 @@ function show_rune(evt) {
   let id = evt.currentTarget.i;
 
   del_panel_rune('.second-rune');
-  del_panel_rune('.secondary-runes'); // FIX: fix veloce per il bug della pagina principale = a secondaria
+  del_panel_rune('.secondary-runes');
+  setPlaceholder('.secondary-runes'); // FIX: fix veloce per il bug della pagina principale = a secondaria
 
+  // stampa 4 rune su 5 per le secondarie
   for (let i = 0; i < p_rune.length; i++) {
     if (i == id) continue;
 
@@ -112,6 +113,7 @@ function show_rune(evt) {
   // creo la variabile runeTree per rendere più chiaro il codice
   let runeTree = p_rune[id];
 
+  // stampa tutto il ramo di rune primarie selezionato
   for (let i = 0; i < rune[runeTree].length; i++) {
     for (let j = 0; j < rune[runeTree][i].length; j++) {
       let label = document
@@ -146,6 +148,7 @@ function show_snd_rune(evt) {
   // creo la variabile runeTree per rendere più chiaro il codice
   let runeTree = p_rune[id];
 
+  // stampa tutto il ramo di rune secondarie selezionato
   for (let i = 1; i < rune[runeTree].length; i++) {
     for (let j = 0; j < rune[runeTree][i].length; j++) {
       let label = document
@@ -177,6 +180,26 @@ function del_panel_rune(panel_target) {
   rune_to_dl = document.querySelector(panel_target);
   while (rune_to_dl.hasChildNodes())
     rune_to_dl.removeChild(rune_to_dl.firstChild);
+}
+
+function setPlaceholder(panel_target) {
+  const label = document.createElement('label');
+  label.className = 'placeholder';
+  const img = document.createElement('img');
+  img.src = './img/runesReforged/perk/qm.png';
+  label.append(img);
+
+  const div = document.createElement('div');
+  div.className = 'break';
+
+  const target = document.querySelector(panel_target);
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      target.append(label.cloneNode(true));
+    }
+    target.append(div.cloneNode(true));
+  }
 }
 
 function manage_secondary_runes(radio_name) {
